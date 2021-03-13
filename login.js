3e890e5d4d34cc9966b375fa6d12e9d1f5c54b8e
Davide Morsia <davidemorsia@yahoo.it>
2021-03-13 14:14:43 +0100

    Second Commit

--- login.js
+++ login.js
@@ -87,4 +87,7 @@ app.post('/registration', function(request, response) {
 	}
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server is running on port ${PORT}.');
+});
